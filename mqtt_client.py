c57e94eb95924faf94294d9dc308c5ab885361ec
PhuongNamCode <phuongnamvp166@gmail.com>
2023-06-03 11:03:58 +0700

    v4

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,12 +1,17 @@
 from  Adafruit_IO import  MQTTClient
+from dotenv import load_dotenv
+import dotenv
 import random
 import time
 import sys
+import os
+load_dotenv()
 class Mqtt:
     client = None
     def __init__(self):
-        AIO_USERNAME = "PhuongNamvp160601"
-        AIO_KEY = "aio_LcDr77HvneWUX410CEXxZFHJkoIV"
+        AIO_USERNAME = os.getenv('NAME')
+        AIO_KEY = os.getenv('KEY')
+        print("hi", AIO_USERNAME, AIO_KEY)
         self.client = MQTTClient(AIO_USERNAME , AIO_KEY)
         self.client.connect()
         self.client.loop_background()
